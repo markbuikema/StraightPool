e1bf3ff6edc91905beca1b82a0c26651eada6af9
Mark Buikema <mark_buikema@hotmail.com>
2012-10-24 03:14:16 +0200

    Working contacts import, hackerproof i think

--- src/com/markbuikema/straightpool/CreateProfileActivity.java
+++ src/com/markbuikema/straightpool/CreateProfileActivity.java
@@ -1,7 +1,9 @@
 package com.markbuikema.straightpool;
 
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -16,10 +18,10 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds;
-import android.provider.ContactsContract.Contacts.Data;
+import android.provider.ContactsContract.CommonDataKinds.Event;
 import android.provider.ContactsContract.PhoneLookup;
 import android.support.v4.content.CursorLoader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -176,67 +178,123 @@ public class CreateProfileActivity extends Activity {
 		}
 	}
 
-	@SuppressWarnings("deprecation")
 	public void onActivityResult(int reqCode, int resultCode, Intent data) {
 		super.onActivityResult(reqCode, resultCode, data);
 
 		switch (reqCode) {
 		case (PICK_CONTACT):
-			if (resultCode == Activity.RESULT_OK) {
-				Uri contactData = data.getData();
-				String[] projection = new String[] {ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts._ID};
-				Cursor c = managedQuery(contactData, projection, null, null, null);
-				startManagingCursor(c);
-				if (c.moveToFirst()) {
+			try {
+				if (resultCode == Activity.RESULT_OK) {
+					String displayName = "0";
+					Uri contactData = data.getData();
+					Log.d("URI:", contactData.toString());
+					Cursor c = managedQuery(contactData, null, null, null, null);
+					if (c.moveToFirst()) {
+						displayName = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+						firstNameView.setText(displayName);
+					}
 
-					String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
-	        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-					firstNameView.setText(name);
+					// part2
 
-          Toast.makeText(getApplicationContext(),name , Toast.LENGTH_SHORT).show();
+					ContentResolver cr = CreateProfileActivity.this.getContentResolver();
+					String[] projection = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME };
 
-	        
-	        ContentResolver bd = getContentResolver();
-	        String where = Data.RAW_CONTACT_ID+" = "+id+" and "+Data.MIMETYPE+" = "+CommonDataKinds.Event.CONTENT_ITEM_TYPE;
-	        Cursor bdc = bd.query(ContactsContract.Data.CONTENT_URI, new String[] {CommonDataKinds.Event.START_DATE}, where, null, null);
-	        if (bdc.getCount() > 0) {
-	            while (bdc.moveToNext()) {
-	                String birthday = bdc.getString(0);
-	                Toast.makeText(getApplicationContext(), id+name+birthday, Toast.LENGTH_SHORT);
-	            }
-	        }
-					Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+					Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, projection, null, null, ContactsContract.Contacts.DISPLAY_NAME
+							+ " COLLATE LOCALIZED ASC");
+
+					while (cur.moveToNext()) {
+
+						String columns[] = { ContactsContract.CommonDataKinds.Event.START_DATE, ContactsContract.CommonDataKinds.Event.TYPE,
+								ContactsContract.CommonDataKinds.Event.MIMETYPE, };
+
+						String where = Event.TYPE + "=" + Event.TYPE_BIRTHDAY + " and " + Event.MIMETYPE + " = '" + Event.CONTENT_ITEM_TYPE + "' and "
+								+ ContactsContract.Data.DISPLAY_NAME + " = '" + displayName + "'";
+
+						String[] selectionArgs = null;
+						String sortOrder = ContactsContract.Contacts.DISPLAY_NAME;
+
+						Cursor birthdayCur = cr.query(ContactsContract.Data.CONTENT_URI, columns, where, selectionArgs, sortOrder);
+						if (birthdayCur.getCount() > 0) {
+							while (birthdayCur.moveToNext()) {
+								int bdayIndex = birthdayCur.getColumnIndex(ContactsContract.CommonDataKinds.Event.START_DATE);
+								if (bdayIndex == -1) {
+									birthDate = null;
+									birthDateView.setText("");
+								} else {
+								String birthday = birthdayCur.getString(bdayIndex);
+
+								if (birthDate == null) {
+									birthDate = new GregorianCalendar();
+								}
+
+								birthDate.set(GregorianCalendar.DAY_OF_MONTH, Integer.valueOf(birthday.split("-")[2]));
+								birthDate.set(GregorianCalendar.MONTH, Integer.valueOf(birthday.split("-")[1]) - 1);
+								birthDate.set(GregorianCalendar.YEAR, Integer.valueOf(birthday.split("-")[0]));
+								String dateString = birthDate.get(GregorianCalendar.DAY_OF_MONTH) + " "
+										+ birthDate.getDisplayName(GregorianCalendar.MONTH, GregorianCalendar.LONG, Locale.US) + " "
+										+ birthDate.get(GregorianCalendar.YEAR);
+								birthDateView.setText(dateString);
+								}
+							}
+						}
+						birthdayCur.close();
+
+					}
+
+					cur.close();
+
+					// part3;
+
+					String[] proj = new String[] { ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME,
+							ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME, ContactsContract.CommonDataKinds.StructuredName.MIDDLE_NAME,
+							ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME };
+
+					String selectionArgs = ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME + "='" + displayName + "'";
+					Cursor cursor = getContentResolver().query(ContactsContract.Data.CONTENT_URI, proj, selectionArgs, null, null);
+
+					int indexGivenName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+					int indexFamilyName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+					int indexDisplayName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME);
+					int indexMiddleName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.MIDDLE_NAME);
+
+					Log.d("Cursor size", cursor.getCount() + ".");
+					cursor.moveToFirst();
+
+					// while (cursor.moveToNext()) {
+					Log.d("tag", "nu in whileloopje");
+					String given = "-";
+					String family = "-";
+					String middle = "-";
+					if (indexGivenName != -1) {
+						given = cursor.getString(indexGivenName);
+					}
+					if (indexFamilyName != -1) {
+						family = cursor.getString(indexFamilyName);
+					}
+					if (indexMiddleName != -1) {
+						middle = cursor.getString(indexMiddleName);
+					}
+					String display = cursor.getString(indexDisplayName);
+					Log.d("names", given + "," + middle + "," + family + "," + display);
+
+					if (middle == null) {
+						firstNameView.setText(given);
+						lastNameView.setText(family);
+						Log.d("given name: ", given);
+					} else {
+						firstNameView.setText(given);
+						lastNameView.setText(middle + " " + family);
+					}
+					// }
+
 				}
+			} catch (Exception e) {
+				Toast.makeText(CreateProfileActivity.this, "Exception logged", Toast.LENGTH_SHORT).show();
+				e.printStackTrace();
 			}
 			break;
 		}
-		
-		
 
-	}
-	
-	
-
-	// method to get name, contact id, and birthday
-	@SuppressWarnings("deprecation")
-	private Cursor getContactsBirthdays() {
-		Uri uri = ContactsContract.Data.CONTENT_URI;
-
-		String[] projection = new String[] { ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.CommonDataKinds.Event.CONTACT_ID,
-				ContactsContract.CommonDataKinds.Event.START_DATE };
-
-		String where = ContactsContract.Data.MIMETYPE + "= ? AND " + ContactsContract.CommonDataKinds.Event.TYPE + "="
-				+ ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY;
-		String[] selectionArgs = new String[] { ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE };
-		String sortOrder = null;
-		CursorLoader cl = new CursorLoader(CreateProfileActivity.this);
-		cl.setProjection(projection);
-		cl.setSelection(where);
-		cl.setSelectionArgs(selectionArgs);
-		cl.setSortOrder(sortOrder);
-		cl.setUri(uri);
-
-		return cl.loadInBackground();
 	}
 
 	private Drawable getIconFromPackageName(final String packageName) {
